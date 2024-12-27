16db868bb98a67e704a4d4709c94085b01a4470f
Simone Celia <simone.celia@eng.it>
2024-12-27 20:19:03 +0100

    fix 4 proxy

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
-$file = fopen ('ip.log', 'a+');
-fwrite($file, $_SERVER['REMOTE_ADDR']."\r\n");
+// Verifica se l'intestazione X-Forwarded-For è presente
+if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+	// Se presente, prendi il primo IP (nel caso di più proxy)
+	$ipAddress = strtok($_SERVER['HTTP_X_FORWARDED_FOR'], ',');
+} else {
+	// Altrimenti prendi l'IP direttamente da REMOTE_ADDR
+	$ipAddress = $_SERVER['REMOTE_ADDR'];
+}
+
+// Visualizza l'indirizzo IP
+// echo "L'indirizzo IP del client è: " . $ipAddress;
+
+
+$file = fopen('ip.log', 'a+');
+fwrite($file, $ipAddress . "\r\n");
 
 header("Location: https://www.google.it/");
 die();
-?>
